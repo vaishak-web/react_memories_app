229b954b5437cd2d3cb73db763985c3768164e39
Vaishak Nair <vaishak.web@gmail.com>
2021-07-09 01:50:33 +0530

    structure and component

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post/Post'
+import useStyles from './styles'
+
+const Posts = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            <h1>Posts</h1>
+            <Post />
+        </div>
+    )
+}
+
+export default Posts
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -13,9 +13,10 @@ export const createPost = async (req, res) => {
     const body = req.body;
     const newPost = new PostMessage(post);
     try{
-        await newPost.save()
+        await newPost.save();
+        res.status(201).json(newPost);
     }catch(error){
-        
+        res.status(409),json({message:error.message});
     }
 
 };
