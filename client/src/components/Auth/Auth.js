0611f60e2c31f9e792f152959f4287063b9e9fc5
Vaishak Nair <vaishak.web@gmail.com>
2021-07-13 01:57:38 +0530

    auth setup

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Avatar, Grid, Container, TextField, Button, Typography, Paper } from "@material-ui/core";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from "./styles";
 
 const Auth = () => {
     const classes = useStyles();
+    const isSignup = false;
+    const handleSubmit = () =>{};
+    const handleChange = () =>{};
+
     return (
         <Container component="main" maxWidth="xs">
           <Paper className={classes.paper} elevation={3}>
             <Avatar className={classes.avatar}>
-                </Avatar>     
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+            <form className={classes.form} onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                    {
+                        isSignup && (
+                            <>
+                                <TextField name="firstName" label="First Name" handleChange={handleChange} autoFocus xs={6} />
+                            </>
+                        )
+                    }
+                </Grid>
+            </form>
          </Paper>  
         </Container>
     )
